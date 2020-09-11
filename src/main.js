e2341cf0b7cb547509ad7475d08d6399ef99027f
jd <duverseaujohnny21@gmail.com>
2020-09-11 17:28:29 -0400

    add condition

--- src/main.js
+++ src/main.js
@@ -8,13 +8,32 @@ import CurrencyService from './currency-exchanger.js'
 // function
 function displayCurrencyResults(currencyResponse) { 
   if(currencyResponse) {
-    
+
+      $('#currencyShow').append(`The currency is ${currencyResponse.base_code.rates}`); 
+      
   } else {
-    $('.showErrors').text(`There was an error: ${currencyResponse.message}`);
+    $('.showErrors').text(`This currency is not existed error: ${currencyResponse.message}`);
   }
   }
   
 
+  // Dom
+$(document).ready(function() {
+
+  $('#currencyInfo').click(function(event) {
+      event.preventDefault();
+      const number = $('#number').val();
+      const country = $('#country').val();
+      
+      // clearFields();
+      $('#currencyShow').html("");
+      CurrencyService.getId(number, country)
+        .then(function(currencyResponse) {
+          displayCurrencyResults(currencyResponse);
+        });
+        console.log("hello")
+    })
+  });        
 
 
 
