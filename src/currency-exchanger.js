45cb38fe7bb8c7ebfc2ce21d2f74eda7de3e4189
jd <duverseaujohnny21@gmail.com>
2020-09-11 17:44:29 -0400

    add the currency code

---
+++ src/currency-exchanger.js
@@ -0,0 +1,18 @@
+export default class CurrencyService {  
+  static getId(number,country) {
+      console.log(`getId function running. number: ${number},country: ${country}`)
+    return fetch(`https://open.exchangerate-api.com/v6/latest?appid=${process.env.API_KEY}&q=${number}&ISO_4217=${country}`)
+      .then(function(currencyResponse) {
+        if (!currencyResponse.ok) {
+          console.log(`Error thrown`);
+          throw Error(currencyResponse.statusText);
+        }
+        console.log(`currencyResponse.Json returned`);
+        return currencyResponse.json();
+      })
+      .catch(function(error) {
+        console.log(`Error caught`);
+        return error;
+      })
+  }
+}
