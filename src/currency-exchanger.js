16864fb8dc889e5671c0fa41052eb6f1118b51e5
jd <duverseaujohnny21@gmail.com>
2020-09-13 16:41:46 -0400

    refactor my code with async function

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -1,18 +1,13 @@
 export default class CurrencyService {  
-  static getId(number, country) {
-      console.log(`getId function running. number: ${number},country: ${country}`)
-    return fetch(`https://open.exchangerate-api.com/v6/latest?appid=${process.env.API_KEY}&base_code=${number}&=country${country}`)
-      .then(function(currencyResponse) {
-        if (!currencyResponse.ok) {
-          console.log(`Error thrown`);
-          throw Error(currencyResponse.statusText);
-        }
-        console.log(`currencyResponse.Json returned`);
-        return currencyResponse.json();
-      })
-      .catch(function(error) {
-        console.log(`Error caught`);
-        return error;
-      })
+  static async getId() {
+    try {
+      const response = await fetch(`https://open.exchangerate-api.com/v6/latest?appid=${process.env.API_KEY}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
