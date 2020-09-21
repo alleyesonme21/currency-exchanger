91f73abb4008b26623b402f75e528a6fcd95edd5
jd <duverseaujohnny21@gmail.com>
2020-09-21 08:47:26 -0400

    indented currency-exchanger .js

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -1,5 +1,5 @@
 export default class CurrencyService {  
-  static async getId() {
+  static async getCurrency() {
     try {
       const response = await fetch(`https://open.exchangerate-api.com/v6/latest?appid=${process.env.API_KEY}`);
       if (!response.ok) {
